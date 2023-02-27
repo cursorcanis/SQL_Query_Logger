fac1ef44a9b5a9f235979607d862b069dcd55937
Alfredo Alea <alfredoalea@gmail.com>
2023-02-27 11:08:23 -0500

    Added the searchstring functions to index.cshtml and index.cshtml.cs

--- SQL_Query_Logger/Pages/Queries/Index.cshtml.cs
+++ SQL_Query_Logger/Pages/Queries/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SQL_Query_Logger.Data;
 using SQL_Query_Logger.Models;
@@ -21,8 +22,27 @@ namespace SQL_Query_Logger.Pages.Queries
 
         public IList<Query> Query { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Types_of_Queries { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Query_Type { get; set; }
+
+
+
         public async Task OnGetAsync()
         {
+            // using System. Linq
+            var queries = from q in _context.Query
+                          select q;
+            if (!string.IsNullOrEmpty(SearchString) ) {
+                queries = queries.Where(s => s.Title_of_Query.Contains(SearchString));
+            }
+
+            Query = await queries.ToListAsync();
+
             if (_context.Query != null)
             {
                 Query = await _context.Query.ToListAsync();
